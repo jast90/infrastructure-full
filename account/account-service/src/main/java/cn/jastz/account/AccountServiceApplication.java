82e6689bdfb17b5201b73a647ad848a587dd39e8
zhangzhiwen <zhangzhiwen91@gmail.com>
2018-05-30 13:49:49 +0800

    spring boot 项目打包成war包

--- account/account-service/src/main/java/cn/jastz/account/AccountServiceApplication.java
+++ account/account-service/src/main/java/cn/jastz/account/AccountServiceApplication.java
@@ -4,31 +4,37 @@ package cn.jastz.account;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.social.connect.ConnectionSignUp;
 import org.springframework.social.connect.web.SignInAdapter;
 
+/**
+ * 为了将项目打包成war包需要继承自SpringBootServletInitializer
+ */
 @SpringBootApplication
 @MapperScan(basePackages = "cn.jastz.*.mapper")
-public class AccountServiceApplication {
+public class AccountServiceApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AccountServiceApplication.class);
+    }
 
     public static void main(String[] args) {
-        SpringApplication.run(AccountServiceApplication.class,args);
+        SpringApplication.run(AccountServiceApplication.class, args);
     }
 
     @Bean
-    public SignInAdapter signInAdapter(){
+    public SignInAdapter signInAdapter() {
         System.out.println("signInAdapter init");
         return new MySignInAdapter();
     }
 
 
     @Bean
-    public ConnectionSignUp connectionSignUp(){
+    public ConnectionSignUp connectionSignUp() {
         return new MyConnectionSignUp();
     }
-
-
-
-
 }
