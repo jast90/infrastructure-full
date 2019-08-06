082744445bb48171f9f5cb370b9fb4f394f86989
jast90 <zhangzhiwen91@gmail.com>
2019-08-06 22:52:45 +0800

    swagger boot

---
+++ swagger-spring-boot/swagger-spring-boot-autoconfigure/src/main/java/me/jastz/swagger/spring/boot/autoconfig/EnableSwagger.java
@@ -0,0 +1,19 @@
+package me.jastz.swagger.spring.boot.autoconfig;
+
+import org.springframework.context.annotation.Import;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.configuration.Swagger2DocumentationConfiguration;
+
+import java.lang.annotation.*;
+
+/**
+ * @author zhiwen
+ * @date 2017/10/27
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@Import({Swagger2DocumentationConfiguration.class, SwaggerAutoConfiguration.class})
+public @interface EnableSwagger {
+}
---
+++ swagger-spring-boot/swagger-spring-boot-autoconfigure/src/main/java/me/jastz/swagger/spring/boot/autoconfig/SwaggerAutoConfiguration.java
@@ -0,0 +1,139 @@
+package me.jastz.swagger.spring.boot.autoconfig;
+
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+
+import com.google.common.collect.Lists;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.OAuthBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
+
+/**
+ * 用于创建Docket ,SecurityConfiguration 和UiConfiguration
+ *
+ * @author zhiwen
+ * @date 2017/10/27
+ */
+@EnableConfigurationProperties(SwaggerProperties.class)
+public class SwaggerAutoConfiguration {
+
+    private final SwaggerProperties properties;
+
+    private static final String API_KEY = "api_key";
+    private static final String OAUTH2 = "oauth2";
+
+    public SwaggerAutoConfiguration(SwaggerProperties properties) {
+        this.properties = properties;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public Docket docket() {
+        List<Predicate<String>> excludePath = new ArrayList();
+        for (String path : properties.getExcludePath()) {
+            excludePath.add(PathSelectors.ant(path));
+        }
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .pathMapping(properties.getPathMapping())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(properties.getBasePackage()))
+                .paths(Predicates.and(Predicates.not(Predicates.or(excludePath))))
+                .build()
+                .securitySchemes(newArrayList(securityScheme()))
+                .securityContexts(newArrayList(oauth()))
+                .apiInfo(apiInfo())
+                ;
+    }
+
+    private SecurityScheme securityScheme() {
+        if (API_KEY.equals(properties.getScheme())) {
+            return apiKey();
+        } else if (OAUTH2.equalsIgnoreCase(properties.getScheme())) {
+            return new OAuthBuilder().name("oauth2").grantTypes(grantTypes())
+                    .scopes(scopes()).build();
+        } else {
+            throw new IllegalArgumentException("Not support scheme.");
+        }
+    }
+
+
+    private List<AuthorizationScope> scopes() {
+
+        List<AuthorizationScope> authorizationScopes = Lists.newArrayList();
+        if (OAUTH2.equals(properties.getScheme())) {
+            for (String scope : properties.getScopeList()) {
+                authorizationScopes.add(new AuthorizationScope(scope, scope));
+            }
+        }
+        return authorizationScopes;
+    }
+
+    private List<GrantType> grantTypes() {
+        String tokenUrl = properties.getGrantType().getTokenUrl(), authorizeUrl = properties.getGrantType().getAuthorizeUrl(), loginUrl = properties.getGrantType().getLoginUrl();
+        List<GrantType> grantTypes = new ArrayList<>();
+        if ("authorization_code".equals(properties.getGrantType().getName())) {
+            AuthorizationCodeGrant authorizationCodeGrant = new AuthorizationCodeGrant(
+                    new TokenRequestEndpoint(authorizeUrl, properties.getOauth2().getClientId()
+                            , properties.getOauth2().getSecretId()),
+                    new TokenEndpoint(tokenUrl, "access_token"));
+            grantTypes.add(authorizationCodeGrant);
+        } else if ("client_credentials".equals(properties.getGrantType().getName())) {
+            ClientCredentialsGrant clientCredentialsGrant = new ClientCredentialsGrant(tokenUrl);
+            grantTypes.add(clientCredentialsGrant);
+        } else if ("password".equals(properties.getGrantType().getName())) {
+            ResourceOwnerPasswordCredentialsGrant resourceOwnerPasswordCredentialsGrant =
+                    new ResourceOwnerPasswordCredentialsGrant(tokenUrl);
+            grantTypes.add(resourceOwnerPasswordCredentialsGrant);
+        } else if ("implicit".equals(properties.getGrantType().getName())) {
+            ImplicitGrant implicitGrant = new ImplicitGrant(new LoginEndpoint(tokenUrl), "access_token");
+            grantTypes.add(implicitGrant);
+        }
+        return grantTypes;
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("mykey", properties.getParam().getName(), properties.getParam().getType());
+    }
+
+
+    private SecurityContext oauth() {
+        List<SecurityReference> auth;
+        if (API_KEY.equals(properties.getScheme())) {
+            auth = apiAuth();
+        } else {
+            auth = oauth2Auth();
+        }
+        return SecurityContext.builder()
+                .securityReferences(auth)
+                .forPaths(PathSelectors.ant("/**"))
+                .build();
+    }
+
+    private List<SecurityReference> apiAuth() {
+        return newArrayList(
+                new SecurityReference("mykey", scopes().toArray(new AuthorizationScope[0])));
+    }
+
+    private List<SecurityReference> oauth2Auth() {
+        return newArrayList(
+                new SecurityReference("oauth2", scopes().toArray(new AuthorizationScope[0])));
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title(properties.getTitle()).description(properties.getDescription()).version(properties.getVersion()).build();
+    }
+}
